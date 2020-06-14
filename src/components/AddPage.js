945196a278a2df3bc11b7facd379893b7831932e
unknown <novenkaya1907@mail.ru>
2020-06-14 15:45:01 +0300

    reset for add and edit

--- src/components/AddPage.js
+++ src/components/AddPage.js
@@ -31,7 +31,7 @@ function AddPage(props) {
 
   const submitAdd = (values) => {
     saveRecipe(values);
-    //history.push("/showAllRecipes")
+    history.push("/showAllRecipes")
   }
 
   const submitEdit = (values) => {
@@ -42,6 +42,16 @@ function AddPage(props) {
     if (match.params.hasOwnProperty('recipeId')) submitEdit(values)
     else submitAdd(values)
     methods.resetForm();
+  }
+  const reset =() => {
+    if (match.params.hasOwnProperty('recipeId')) resetEdit()
+    else resetAdd()
+  }
+  const  resetAdd = () =>{
+    history.push("/")
+  }
+  const resetEdit = (clean) => {
+    openModal()
   }
   const cleanForm = (clean) => {
     clean();
@@ -59,33 +69,31 @@ function AddPage(props) {
         validationSchema={BasicFormSchema}
         onSubmit={submit}>
 
-        {({ isSubmitting, errors, touched, resetForm }) =>  { 
-          return(
-          
-          <Form className='AddForm'>
-            <Modal
-              isOpen={modalOpen}
-              onRequestClose={closeModal}
-              contentLabel="Example Modal"
-              style={customStyles}
-              ariaHideApp={false}
-              
-            >   <div className='modal'>
-                <div className="recipeTitle">Вы уверены, что хотите отменить редактирование?</div>
-                <button className='recipeButton' onClick={cleanForm.bind(this, resetForm)} >Да, уверен</button>
-                <button className='recipeButton' onClick={closeModal}>Отмена</button>
-              </div>
-            </Modal>
-            <label htmlFor="title">Название рецепта</label>
-            <Field name="title" type='text' placeholder='Название рецепта' />
-            {errors.title && touched.title ? (<div className="error">{errors.title}</div>) : null}
-            <label htmlFor='description'>Описание рецепта </label>
-            <Field component='textarea' name="description" placeholder='Описание рецепта' />
-            {errors.description && touched.description ? (<div className="error">{errors.description}</div>) : null}
-            <button className='recipeButton' type="submit" disabled={isSubmitting}>Сохранить</button>
-            <button className='recipeButton' type='button' onClick={openModal}>Отменить</button>
-          </Form>
-        )}}
+        {({ isSubmitting, errors, touched, resetForm }) =>
+          (
+            <Form className='AddForm'>
+              <Modal
+                isOpen={modalOpen}
+                onRequestClose={closeModal}
+                contentLabel="Example Modal"
+                style={customStyles}
+                ariaHideApp={false}>
+                <div className='modal'>
+                  <div className="recipeTitle">Вы уверены, что хотите отменить редактирование?</div>
+                  <button className='recipeButton' onClick={cleanForm.bind(this, resetForm)} >Да, уверен</button>
+                  <button className='recipeButton' onClick={closeModal}>Отмена</button>
+                </div>
+              </Modal>
+              <label htmlFor="title">Название рецепта</label>
+              <Field name="title" type='text' placeholder='Название рецепта' />
+              {errors.title && touched.title ? (<div className="error">{errors.title}</div>) : null}
+              <label htmlFor='description'>Описание рецепта </label>
+              <Field component='textarea' name="description" placeholder='Описание рецепта' />
+              {errors.description && touched.description ? (<div className="error">{errors.description}</div>) : null}
+              <button className='recipeButton' type="submit" disabled={isSubmitting}>Сохранить</button>
+              <button className='recipeButton' type='button' onClick={reset}>Отменить</button>
+            </Form>
+          )}
       </Formik>
     </div>
   )
